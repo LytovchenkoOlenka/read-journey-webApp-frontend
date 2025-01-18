f80fb86d3e293370a5c351c146945647938ef694
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2025-01-18 21:47:44 +0100

    add_routes_&_redux

---
+++ src/redux/auth/authSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    user: null,
+    token: null,
+};
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        setUser: (state, action) => {
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+        },
+        clearUser: (state) => {
+            state.user = null;
+            state.token = null;
+        }
+    }
+    // extraReducers: (builder)=>{}}
+})
+
+export const { setUser, clearUser } = authSlice.actions;
+
+export const authReducer = authSlice.reducer;
