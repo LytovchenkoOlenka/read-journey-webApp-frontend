00d620ae047e377ac26b24d2f1fca16dc342f5ff
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2025-01-19 21:13:46 +0100

    updated_app

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 // import { useState } from 'react'
 import'./App.css'
 import { Routes, Route } from "react-router-dom";
-import RegisterForm from './components/RegisterForm/RegisterForm.jsx';
+import RegisterPage from './pages/RegisterPage/RegisterPage.jsx';
+import Layout from './components/Layout/Layout.jsx';
 
 
 function App() {
 
-  const Home = () => <h1>Home Page</h1>;
-  const Register = () => <RegisterForm />;
+  // const Home = () => <h1>Home Page</h1>;
+  const Register = () => <RegisterPage />;
   const Login = () => <h1>Login Page</h1>;
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path="/" element={<Layout />}>
+        {/*<Route path='/' element={<Home/>}/>*/}
         <Route path='/register' element={<Register />}/>
         <Route path='/login' element={<Login/>}/>
+        </Route>
+
       </Routes>
 
     </>
