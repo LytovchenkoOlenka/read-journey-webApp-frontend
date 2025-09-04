f497a4889988fe46cce3d1a5b4ef3d69bfbec33c
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2025-09-04 20:49:53 +0200

    update_button_comp(tailwind)

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,15 @@
-import css from './Button.module.css'
+// import css from './Button.module.css'
 
 export default function Button({ label, onClick }) {
-    return (
-        <button className={css.button} onClick={onClick}>
-            {label}
-        </button>
-    );
+  return (
+    <button
+      className="bg-white text-charcoal rounded-[30px] border border-transparent p-3 min-w-[130px] font-bold text-sm leading-[1.29] tracking-wide text-center
+        transition-colors duration-500 ease-in-out
+        hover:bg-transparent hover:text-white hover:border-white/20
+        active:bg-transparent active:text-white active:border-white/20"
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
 }
