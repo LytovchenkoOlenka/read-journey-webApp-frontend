f67f4fbce11cb3119ebcb4cabb8124617f73de6a
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2025-09-04 21:38:18 +0200

    update_RegisterForm(tailwind+fixes)

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,76 +1,75 @@
-// import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import css from '../Auth/FormStyles.module.css';
-import * as yup from 'yup';
-
-import Button from '../Button/Button.jsx'
-// import {useState} from "react";
-import Input from '../Input/Input.jsx'
-import {useForm} from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import Button from "../Button/Button.jsx";
+import Input from "../Input/Input.jsx";
 
 // Валідація за допомогою Yup
 const schema = yup.object().shape({
-    name: yup.string().required('Name is required'),
-    email: yup.string().email('Invalid email').required('Email is required'),
-    password: yup.string().min(7, 'Password must be at least 7 characters').required('Password is required'),
+  name: yup.string().required("Name is required"),
+  email: yup.string().email("Invalid email").required("Email is required"),
+  password: yup
+    .string()
+    .min(7, "Password must be at least 7 characters")
+    .required("Password is required"),
 });
 
-export default function RegisterForm(){
-    const {
-        handleSubmit,
-        setValue,
-        watch,
-        formState: { errors },
-    } = useForm({resolver: yupResolver(schema),});
+export default function RegisterForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema), mode: "onChange" });
 
-    const onSubmit = (data) => {
-        console.log(data);
-    };
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
-    return (
-        <form className={css.form} onSubmit={handleSubmit(onSubmit)}>
-            <div className={css.formWrapper}>
-                <div>
-                    <Input
-                        // label="Name:"
-                        name="name"
-                        type="text"
-                        placeholder="Name:Ilona Ratushniak"
-                        value={watch("name") || ""}
-                        onChange={(e) => setValue("name", e.target.value)}
-                    />
-                    {errors.name && <p className="error">{errors.name.message}</p>}
-                </div>
-                <div>
-                    <Input
-                        // label="Mail:"
-                        name="email"
-                        type="email"
-                        placeholder="Mail: your@email.com"
-                        value={watch("email") || ""}
-                        onChange={(e) => setValue("email", e.target.value)}
-                    />
-                    {errors.email && <p className="error">{errors.email.message}</p>}
-                </div>
-                <div>
-                    <Input
-                        // label="Password:"
-                        name="password"
-                        type="password"
-                        placeholder="Password: Yourpasswordhere"
-                        value={watch("password") || ""}
-                        onChange={(e) => setValue("password", e.target.value)}
-                        withIcon
-                    />
-                    {errors.password && <p className="error">{errors.password.message}</p>}
-                </div>
-            </div>
-            <div className={css.submitBlock}>
-                    <Button
-                        type="submit" // Кнопка автоматично викликає сабміт при натисканні Enter
-                        label="Registation"
-                    />
-                    <a className={css.link} href="/login">Don’t have an account?</a>
-                </div>
-        </form>)
+  return (
+    <form
+      className="flex flex-col gap-5 justify-between mb-10"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="flex flex-col gap-2">
+        <div>
+          <Input label="Name:" name="name" type="text" {...register("name")} />
+          {errors.name && <p className="error">{errors.name.message}</p>}
+        </div>
+        <div>
+          <Input
+            label="Mail:"
+            name="email"
+            type="email"
+            {...register("email")}
+          />
+          {errors.name && (
+            <p className="text-red-500 text-xs mt-1">{errors.name.message}</p>
+          )}
+        </div>
+        <div>
+          <Input
+            label="Password:"
+            name="password"
+            type="password"
+            {...register("password")}
+          />
+          {errors.password && (
+            <p className="error">{errors.password.message}</p>
+          )}
+        </div>
+      </div>
+      <div className="flex items-center gap-3.5">
+        <Button type="submit">Registration</Button>
+        <Link
+          to="/login"
+          className="text-xs leading-[1.17] tracking-tight 
+    underline text-gray-medium 
+    hover:text-white transition-colors duration-300"
+        >
+          Already have an account?
+        </Link>
+      </div>
+    </form>
+  );
 }
