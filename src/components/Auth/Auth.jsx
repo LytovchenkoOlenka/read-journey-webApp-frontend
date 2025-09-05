b289bc4d59533570b4b1bc265a404c972cbf8d21
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2025-09-05 19:57:58 +0200

    set_image_for_breakpoints

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,5 +1,5 @@
-import css from "./Auth.module.css";
-import iphone from "../../img/iPhone.png";
+import iphoneMobile from "../../img/iphone-mobile.png";
+import iphoneDesktop from "../../img/iphone-desktop.png";
 import Frame from "../Frame/Frame.jsx";
 import Logo from "../Logo/Logo.jsx";
 // import Button from '../Button/Button.jsx'
@@ -17,6 +17,14 @@ export default function Auth({ children }) {
       </Frame>
       <Frame>
         <img src={iphone} alt="Phone" className={css.phoneImg} />
+        <picture>
+          <source media="(min-width: 1280px)" srcSet={iphoneDesktop} />
+          <img
+            src={iphoneMobile}
+            alt="Phone"
+            className="w-full h-full object-cover"
+          />
+        </picture>
       </Frame>
     </>
   );
