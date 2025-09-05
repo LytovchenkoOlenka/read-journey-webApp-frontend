407379bedffe5e4f29fed5ae0b1e01b99bb4c2fb
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2025-09-05 20:00:49 +0200

    breakpoints_for_button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,15 +1,14 @@
-// import css from './Button.module.css'
-
-export default function Button({ label, onClick }) {
+export default function Button({ children, onClick }) {
   return (
     <button
-      className="bg-white text-charcoal rounded-[30px] border border-transparent p-3 min-w-[130px] font-bold text-sm leading-[1.29] tracking-wide text-center
+      className="bg-white text-charcoal rounded-[30px] border border-transparent py-3 px-7 font-bold text-sm leading-[1.3] text-center
         transition-colors duration-500 ease-in-out
         hover:bg-transparent hover:text-white hover:border-white/20
-        active:bg-transparent active:text-white active:border-white/20"
+        active:bg-transparent active:text-white active:border-white/20
+        tablet:py-4  tablet:px-12 tablet:text-xl tablet:leading-none"
       onClick={onClick}
     >
-      {label}
+      {children}
     </button>
   );
 }
