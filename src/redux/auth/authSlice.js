604be53da20f924390fc6207b9f971625234152b
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2025-09-08 21:04:21 +0200

    add_slice_and_operation_for_registering

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,26 +1,48 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { register } from "./operations";
+
+const handlePending = (state) => {
+  state.isLoading = true;
+  state.error = null;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
 
 const initialState = {
-    user: null,
-    token: null,
+  user: { name: null, email: null },
+  token: null,
+  refreshToken: null,
+  isLoggedIn: false,
+  isRefreshing: false,
+
+  isLoading: false,
+  error: null,
 };
 
 const authSlice = createSlice({
-    name: "auth",
-    initialState,
-    reducers: {
-        setUser: (state, action) => {
-            state.user = action.payload.user;
-            state.token = action.payload.token;
-        },
-        clearUser: (state) => {
-            state.user = null;
-            state.token = null;
-        }
-    }
-    // extraReducers: (builder)=>{}}
-})
+  name: "auth",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, handlePending)
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.isLoggedIn = true;
+        state.user = {
+          name: action.payload.name,
+          email: action.payload.email,
+        };
+        state.token = action.payload.token;
+        state.refreshToken = action.payload.refreshToken;
+      })
+      .addCase(register.rejected, handleRejected);
+  },
+});
 
-export const { setUser, clearUser } = authSlice.actions;
+// export const {} = authSlice.actions;
 
 export const authReducer = authSlice.reducer;
---
+++ src/redux/auth/operations.js
@@ -0,0 +1,22 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://readjourney.b.goit.study/api";
+
+const setAuthHeader = (token) => {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+};
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async (newUser, thunkAPI) => {
+    try {
+      const response = await axios.post("/users/signup", newUser);
+      setAuthHeader(response.data.token);
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.message);
+    }
+  }
+);
