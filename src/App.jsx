1d32bc7b42394b6d6b013f0a1e6fb46b70bf6ae0
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2025-09-11 21:37:58 +0200

    refactor(App.js): update routes structure

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,21 @@ import LoginPage from "./pages/LoginPage/LoginPage.jsx";
 
 // import WelcomePage from "./pages/WelcomePage/WelcomePage.jsx";
 import RecommendedPage from "./pages/RecommendedPage/RecommendedPage.jsx";
-// import MyLibraryPage from "./pages/MyLibraryPage/MyLibraryPage.jsx";
+import MyLibraryPage from "./pages/MyLibraryPage/MyLibraryPage.jsx";
 // import ReadingPage from "./pages/ReadingPage/ReadingPage.jsx";
 
 function App() {
   // const Home = () => <h1>Home Page</h1>;
-  // const Register = () => <RegisterPage />;
-  // const Login = () => <LoginPage />;
 
   return (
     <>
       <Routes>
+        <Route path="login" element={<LoginPage />} />
         <Route path="/" element={<Layout />}>
           {/* <Route path='/' element={<Home/>}/> */}
           <Route path="register" element={<RegisterPage />} />
-          <Route path="login" element={<LoginPage />} />
           <Route path="recommended" element={<RecommendedPage />} />
+          <Route path="library" element={<MyLibraryPage />} />
         </Route>
       </Routes>
     </>
