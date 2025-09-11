05f91e70af94d26358034b90ec9a4f94a0945f4e
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2025-09-11 21:36:28 +0200

    feat(header): Implement responsive header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,52 @@
+import { useState } from "react";
 import sprite from "../../assets/icons.svg";
 import Logo from "../Logo/Logo";
+import MobileMenu from "../MobileMenu/MobileMenu";
+import Navigation from "../Navigation/Navigation";
+import UserInfo from "../UserInfo/UserInfo";
+import Button from "../Button/Button";
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prevState) => !prevState);
+  };
+
   return (
-    <div className="flex flex-row justify-between rounded-[15px] px-5 py-3 bg-charcoal ">
+    <header className="flex flex-row justify-between rounded-[15px] px-5 py-3 bg-charcoal ">
       <Logo />
-      <div className="flex gap-[10px]">
-        <div className="w-[35px] h-[35px] bg-gray-dark rounded-full border border-white/20 flex items-center justify-center ">
-          <p className="text-base font-bold uppercase">i</p>
-        </div>
+
+      {/* Цей блок з'являється тільки на екранах від 'tablet' і ширше */}
+      <div className="hidden tablet:flex items-center">
+        <Navigation />
+      </div>
+
+      <div className="flex items-center gap-3 tablet:gap-4">
+        <UserInfo />
+
+        {/* Ця кнопка з'являється тільки на екранах від 'tablet' і ширше */}
+        <Button
+          variant="transparent"
+          onClick={() => console.log("Log out action")}
+          className="hidden min-w-[100px] p-2 tablet:block"
+        >
+          Log out
+        </Button>
+
+        {/* Ця кнопка з'являється тільки на мобільних екранах  */}
         <button
           type="button"
-          //   onClick={}
-          className="p-0 bg-transparent border-none cursor-pointer text-[#888] hover:text-white transition"
+          onClick={toggleMenu}
+          className="p-0 bg-transparent border-none tablet:hidden"
         >
-          <svg width={28} height={28}>
+          <svg width={28} height={28} className="stroke-white">
             <use href={`${sprite}#icon-menu`} />
           </svg>
         </button>
       </div>
-    </div>
+
+      <MobileMenu isOpen={isMenuOpen} onClose={toggleMenu} />
+    </header>
   );
 }
---
+++ src/components/UserInfo/UserInfo.jsx
@@ -0,0 +1,7 @@
+export default function UserInfo() {
+  return (
+    <div className="w-[35px] h-[35px] bg-gray-dark rounded-full border border-white/20 flex items-center justify-center tablet:w-10 tablet:h-10  ">
+      <p className="text-base font-bold uppercase">i</p>
+    </div>
+  );
+}
