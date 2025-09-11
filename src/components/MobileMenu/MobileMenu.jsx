e9e9f3b525d8bd44249647128d97005d7460bbd2
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2025-09-11 21:35:54 +0200

    feat(header): Implement slide-out mobile menu

---
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -0,0 +1,42 @@
+import Navigation from "../Navigation/Navigation";
+import sprite from "../../assets/icons.svg";
+import Button from "../Button/Button";
+
+export default function MobileMenu({ isOpen, onClose }) {
+  return (
+    <div
+      className={`
+        fixed inset-0 z-50 transition-opacity duration-300 tablet:hidden
+        ${
+          isOpen
+            ? "opacity-100 pointer-events-auto"
+            : "opacity-0 pointer-events-none"
+        }
+      `}
+    >
+      <div className="absolute inset-0 bg-black/60" onClick={onClose} />
+
+      <div
+        className={`absolute top-0 right-0 bottom-0 min-w-[200px] bg-charcoal pt-[280px] pb-9 px-10  transform transition-transform duration-500 ease-in-out ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <button onClick={onClose} className="absolute top-9 right-10">
+          <svg width={28} height={28}>
+            <use href={`${sprite}#icon-close`} />
+          </svg>
+        </button>
+        <div className="flex flex-col items-center justify-between h-full">
+          <Navigation />
+          <Button
+            variant="transparent"
+            onClick={() => console.log("Log out action")}
+            className="min-w-[100px] p-2"
+          >
+            Log out
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,30 @@
+// import Button from "../Button/Button";
+import { NavLink } from "react-router-dom";
+// import UserInfo from "../UserInfo/UserInfo";
+
+export default function Navigation() {
+  const getNavLinkClass = ({ isActive }) => {
+    const commonClasses =
+      "text-sm tracking-tight transition hover:text-white relative";
+    const activeClasses = "text-white active-nav-link";
+    const inactiveClasses = "text-gray-medium";
+    return `${commonClasses} ${isActive ? activeClasses : inactiveClasses}`;
+  };
+
+  return (
+    <nav>
+      <ul className="flex flex-col gap-5 tablet:flex-row">
+        <li>
+          <NavLink to="/recommended" className={getNavLinkClass}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/library" className={getNavLinkClass}>
+            My library
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
