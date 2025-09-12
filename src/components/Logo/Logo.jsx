928fe7a6499cbc8f1627322502577ee31bc0e3d3
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2025-09-12 20:19:12 +0200

    refactor(logo): Add prop to control text visibility

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 import logo from "/logo.svg";
 
-export default function Logo() {
+export default function Logo({ textClasses = "hidden" }) {
   return (
     <Link to="/" className="flex items-center tablet:gap-1 ">
       <img src={logo} alt="LogoOfTheHeader" />
-      <p className="hidden tablet:block tablet:font-bold tablet:text-lg tablet:leading-none">
+      <p
+        className={`${textClasses} font-bold text-lg leading-nonehidden desktop:font-bold desktop:text-lg desktop:leading-none`}
+      >
         READ JOURNEY
       </p>
     </Link>
