57ced968982e769ef7ec88c6e934e62a6ae85f58
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2025-09-14 21:35:43 +0200

    feat(user): Display dynamic user data from state

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,7 +1,17 @@
+import { useSelector } from "react-redux";
+import { selectUser } from "../../redux/auth/selectors";
+
 export default function UserInfo() {
+  const user = useSelector(selectUser);
+
+  const username = user?.name || "User";
+  const userInitial = username[0];
   return (
-    <div className="w-[35px] h-[35px] bg-gray-dark rounded-full border border-white/20 flex items-center justify-center tablet:w-10 tablet:h-10  ">
-      <p className="text-base font-bold uppercase">i</p>
+    <div className="flex gap-2 items-center">
+      <div className="w-[35px] h-[35px] bg-gray-dark rounded-full border border-white/20 flex items-center justify-center tablet:w-10 tablet:h-10  ">
+        <p className="text-base font-bold uppercase">{userInitial}</p>
+      </div>
+      <p className="hidden desktop:block text-base font-bold">{username}</p>
     </div>
   );
 }
