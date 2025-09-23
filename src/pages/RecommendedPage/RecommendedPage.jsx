b3be2e168aec7482587b08b3fe0dd886101009b9
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2025-09-23 15:34:48 +0200

    add_recommendedBooks_section

--- src/pages/RecommendedPage/RecommendedPage.jsx
+++ src/pages/RecommendedPage/RecommendedPage.jsx
@@ -1,7 +1,20 @@
+import { useDispatch } from "react-redux";
+import Dashboard from "../../components/Dashboard/Dashboard";
+import RecommendedBooks from "../../components/RecommendedBooks/RecommendedBooks";
+import { fetchRecommendedBooks } from "../../redux/books/operations";
+import { useEffect } from "react";
+
 export default function RegisterPage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRecommendedBooks());
+  }, [dispatch]);
+
   return (
-    <div>
-      <h1>RECOMENDED PAGE</h1>
-    </div>
+    <>
+      <Dashboard />
+      <RecommendedBooks />
+    </>
   );
 }
